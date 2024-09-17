276f31679119697cc1fc61d6f3b040bd4e7e4b1f
jeffser <jeffrysamuer@gmail.com>
2024-09-16 21:54:07 -0600

    Added no results and no shimeji screens + custom widgets

---
+++ src/custom_widgets/shimeji_widget.py
@@ -0,0 +1,170 @@
+
+from gi.repository import Adw, Gtk, Gdk
+from colorthief import ColorThief
+import os, shutil
+
+window=None
+
+class shimeji:
+    def __init__(self, shime_name:str):
+        self.instance_id = str(uuid.uuid4())
+        self.shime_name = shime_name
+
+    def work(self):
+        cmd = 'java -classpath Shimeji.jar -Xmx1000m com.group_finity.mascot.Main -Djava.util.logging.config.file=./conf/logging.properties'
+        env = os.environ.copy()
+        env['DISPLAY'] = ':0'
+        working_directory = os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.shime_name)
+
+        process = subprocess.Popen(cmd, shell=True, cwd=working_directory, env=env)
+
+        while not self.stop_event.is_set():
+            if process.poll() is not None:
+                break
+            self.stop_event.wait(1)
+        if process.poll() is None:
+            process.terminate()
+            process.wait()
+
+    def start(self):
+        self.stop_event = threading.Event()
+        self.instance = threading.Thread(target=self.work)
+        self.instance.start()
+
+    def stop(self):
+        self.stop_event.set()
+        self.instance.join()
+
+class shimeji_selector(Gtk.Box):
+    __gtype_name__ = 'GardenShimejiSelector'
+
+    def __init__(self, shime_name:str, window:Gtk.Window):
+        self.window=window
+        super().__init__(
+            css_classes=["card", "shimeji_card"],
+            name=shime_name,
+            valign=1,
+            orientation=1,
+            spacing=10,
+            tooltip_text=shime_name
+        )
+        self.main_color=ColorThief(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name(), 'img', 'shime1.png')).get_color(quality=5)
+        self.main_color='rgb({},{},{})'.format(self.main_color[0], self.main_color[1], self.main_color[2])
+        style_context=self.get_style_context()
+        css_provider=Gtk.CssProvider()
+        css_provider.load_from_data(f".shimeji_card {{\nbackground-color: mix({self.main_color}, @theme_bg_color, .8);\n}}".encode())
+        style_context.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+
+        self.shime_list=[]
+
+        self.title_stack = Gtk.Stack(
+            transition_type=1
+        )
+        self.append(self.title_stack)
+
+        label_container=Gtk.Box(
+            orientation=0,
+            spacing=10
+        )
+
+        self.label=Gtk.Label(
+            label=shime_name,
+            ellipsize=3,
+            can_focus=True,
+            focus_on_click=True,
+            halign=1,
+            hexpand=True
+        )
+        gesture=Gtk.GestureClick.new()
+        gesture.connect('pressed', lambda gesture, n_press, x, y: self.edit_title() if n_press==2 else None)
+        self.label.add_controller(gesture)
+        label_container.append(self.label)
+
+        delete_button=Gtk.Button(
+            icon_name='user-trash-symbolic',
+            css_classes=['flat', 'circular']
+        )
+        delete_button.connect('clicked', self.ask_delete)
+        label_container.append(delete_button)
+        self.title_stack.add_named(label_container, 'title')
+
+        title_entry=Gtk.Entry()
+        title_entry.connect('activate', self.save_name)
+        self.title_stack.add_named(title_entry, 'edit')
+
+        self.image_stack = Gtk.Stack()
+        self.append(self.image_stack)
+
+        image=Gtk.Image.new_from_file(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name(), 'img', 'shime1.png'))
+        image.set_size_request(100, 100)
+        image2=Gtk.Image.new_from_file(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name(), 'img', 'shime1.png'))
+        image2.set_size_request(100, 100)
+        self.image_stack.add_named(image, '1')
+        self.image_stack.add_named(image2, '2')
+
+        spin_button=Gtk.SpinButton.new_with_range(0,10,1)
+        spin_button.set_digits(0)
+        spin_button.set_snap_to_ticks(True)
+        spin_button.set_css_classes(["flat"])
+        spin_button.connect('value-changed', self.value_changed)
+        self.append(spin_button)
+
+    def delete(self):
+        for shime in self.shime_list:
+            shime.stop()
+        shutil.rmtree(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name()))
+        self.get_parent().get_parent().remove(self)
+        if len(list(window.shimeji_container)) == 0:
+            window.window_stack.set_visible_child_name('empty')
+
+    def ask_delete(self, button):
+        dialog=Adw.AlertDialog(
+            heading="Delete Shimeji",
+            body="Are you sure you want to delete '{}'?".format(self.get_name()),
+            close_response="cancel"
+        )
+        dialog.add_response("cancel", "Cancel")
+        dialog.add_response("delete", "Delete")
+        dialog.set_response_appearance("delete", Adw.ResponseAppearance.DESTRUCTIVE)
+        dialog.set_default_response("delete")
+        dialog.choose(
+            parent=self.window,
+            cancellable=None,
+            callback=lambda dialog, task: self.delete() if dialog.choose_finish(task) == 'delete' else None
+        )
+
+    def save_name(self, entry):
+        new_name=entry.get_buffer().get_text().replace('/', '')
+        if new_name:
+            if new_name != self.get_name():
+                compare_list=os.listdir(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji'))
+                if new_name in compare_list:
+                    for i in range(len(compare_list)):
+                        if "{} {}".format(new_name, i+1) not in compare_list:
+                            new_name = "{} {}".format(new_name, i+1)
+                            break
+                os.rename(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name()), os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', new_name))
+                self.set_name(new_name)
+                self.label.set_label(new_name)
+                self.set_tooltip_text(new_name)
+            self.title_stack.set_visible_child_name('title')
+
+    def edit_title(self):
+        self.title_stack.set_visible_child_name('edit')
+        buffer = self.title_stack.get_visible_child().get_buffer()
+        buffer.delete_text(0, -1)
+        buffer.insert_text(0, self.get_name(), len(self.get_name()))
+        self.title_stack.get_visible_child().grab_focus()
+
+    def value_changed(self, spin_button):
+        while spin_button.get_value() != len(self.shime_list):
+            if spin_button.get_value() > len(self.shime_list):
+                self.image_stack.set_transition_type(4)
+                new_shime = shimeji(self.get_name())
+                new_shime.start()
+                self.shime_list.append(new_shime)
+            elif spin_button.get_value() < len(self.shime_list):
+                self.image_stack.set_transition_type(5)
+                self.shime_list.pop(-1).stop()
+
+            self.image_stack.set_visible_child_name('1' if self.image_stack.get_visible_child_name() == '2' else '2')
--- src/window.py
+++ src/window.py
@@ -19,170 +19,8 @@
 
 from gi.repository import Adw, Gtk, Gdk
 import os, threading, uuid, shutil, time, subprocess, re
-from colorthief import ColorThief
 from zipfile import ZipFile
-
-class shimeji:
-    def __init__(self, shime_name:str):
-        self.instance_id = str(uuid.uuid4())
-        self.shime_name = shime_name
-
-    def work(self):
-        cmd = 'java -classpath Shimeji.jar -Xmx1000m com.group_finity.mascot.Main -Djava.util.logging.config.file=./conf/logging.properties'
-        env = os.environ.copy()
-        env['DISPLAY'] = ':0'
-        working_directory = os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.shime_name)
-
-        process = subprocess.Popen(cmd, shell=True, cwd=working_directory, env=env)
-
-        while not self.stop_event.is_set():
-            if process.poll() is not None:
-                break
-            self.stop_event.wait(1)
-        if process.poll() is None:
-            process.terminate()
-            process.wait()
-
-    def start(self):
-        self.stop_event = threading.Event()
-        self.instance = threading.Thread(target=self.work)
-        self.instance.start()
-
-    def stop(self):
-        self.stop_event.set()
-        self.instance.join()
-
-class shimeji_selector(Gtk.Box):
-    __gtype_name__ = 'GardenShimejiSelector'
-
-    def __init__(self, shime_name:str, window:Gtk.Window):
-        self.window=window
-        super().__init__(
-            css_classes=["card", "shimeji_card"],
-            name=shime_name,
-            valign=1,
-            orientation=1,
-            spacing=10,
-            tooltip_text=shime_name
-        )
-        self.main_color=ColorThief(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name(), 'img', 'shime1.png')).get_color(quality=5)
-        self.main_color='rgb({},{},{})'.format(self.main_color[0], self.main_color[1], self.main_color[2])
-        style_context=self.get_style_context()
-        css_provider=Gtk.CssProvider()
-        css_provider.load_from_data(f".shimeji_card {{\nbackground-color: mix({self.main_color}, @theme_bg_color, .8);\n}}".encode())
-        style_context.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
-
-        self.shime_list=[]
-
-        self.title_stack = Gtk.Stack(
-            transition_type=1
-        )
-        self.append(self.title_stack)
-
-        label_container=Gtk.Box(
-            orientation=0,
-            spacing=10
-        )
-
-        self.label=Gtk.Label(
-            label=shime_name,
-            ellipsize=3,
-            can_focus=True,
-            focus_on_click=True,
-            halign=1,
-            hexpand=True
-        )
-        gesture=Gtk.GestureClick.new()
-        gesture.connect('pressed', lambda gesture, n_press, x, y: self.edit_title() if n_press==2 else None)
-        self.label.add_controller(gesture)
-        label_container.append(self.label)
-
-        delete_button=Gtk.Button(
-            icon_name='user-trash-symbolic',
-            css_classes=['flat', 'circular']
-        )
-        delete_button.connect('clicked', self.ask_delete)
-        label_container.append(delete_button)
-        self.title_stack.add_named(label_container, 'title')
-
-        title_entry=Gtk.Entry()
-        title_entry.connect('activate', self.save_name)
-        self.title_stack.add_named(title_entry, 'edit')
-
-        self.image_stack = Gtk.Stack()
-        self.append(self.image_stack)
-
-        image=Gtk.Image.new_from_file(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name(), 'img', 'shime1.png'))
-        image.set_size_request(100, 100)
-        image2=Gtk.Image.new_from_file(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name(), 'img', 'shime1.png'))
-        image2.set_size_request(100, 100)
-        self.image_stack.add_named(image, '1')
-        self.image_stack.add_named(image2, '2')
-
-        spin_button=Gtk.SpinButton.new_with_range(0,10,1)
-        spin_button.set_digits(0)
-        spin_button.set_snap_to_ticks(True)
-        spin_button.set_css_classes(["flat"])
-        spin_button.connect('value-changed', self.value_changed)
-        self.append(spin_button)
-
-    def delete(self):
-        for shime in self.shime_list:
-            shime.stop()
-        shutil.rmtree(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name()))
-        self.get_parent().get_parent().remove(self)
-
-    def ask_delete(self, button):
-        dialog=Adw.AlertDialog(
-            heading="Delete Shimeji",
-            body="Are you sure you want to delete '{}'?".format(self.get_name()),
-            close_response="cancel"
-        )
-        dialog.add_response("cancel", "Cancel")
-        dialog.add_response("delete", "Delete")
-        dialog.set_response_appearance("delete", Adw.ResponseAppearance.DESTRUCTIVE)
-        dialog.set_default_response("delete")
-        dialog.choose(
-            parent=self.window,
-            cancellable=None,
-            callback=lambda dialog, task: self.delete() if dialog.choose_finish(task) == 'delete' else None
-        )
-
-    def save_name(self, entry):
-        new_name=entry.get_buffer().get_text().replace('/', '')
-        if new_name:
-            if new_name != self.get_name():
-                compare_list=os.listdir(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji'))
-                if new_name in compare_list:
-                    for i in range(len(compare_list)):
-                        if "{} {}".format(new_name, i+1) not in compare_list:
-                            new_name = "{} {}".format(new_name, i+1)
-                            break
-                os.rename(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', self.get_name()), os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', new_name))
-                self.set_name(new_name)
-                self.label.set_label(new_name)
-                self.set_tooltip_text(new_name)
-            self.title_stack.set_visible_child_name('title')
-
-    def edit_title(self):
-        self.title_stack.set_visible_child_name('edit')
-        buffer = self.title_stack.get_visible_child().get_buffer()
-        buffer.delete_text(0, -1)
-        buffer.insert_text(0, self.get_name(), len(self.get_name()))
-        self.title_stack.get_visible_child().grab_focus()
-
-    def value_changed(self, spin_button):
-        while spin_button.get_value() != len(self.shime_list):
-            if spin_button.get_value() > len(self.shime_list):
-                self.image_stack.set_transition_type(4)
-                new_shime = shimeji(self.get_name())
-                new_shime.start()
-                self.shime_list.append(new_shime)
-            elif spin_button.get_value() < len(self.shime_list):
-                self.image_stack.set_transition_type(5)
-                self.shime_list.pop(-1).stop()
-
-            self.image_stack.set_visible_child_name('1' if self.image_stack.get_visible_child_name() == '2' else '2')
+from .custom_widgets import shimeji_widget
 
 @Gtk.Template(resource_path='/com/jeffser/Garden/window.ui')
 class GardenWindow(Adw.ApplicationWindow):
@@ -191,6 +29,7 @@ class GardenWindow(Adw.ApplicationWindow):
     shimeji_container = Gtk.Template.Child()
     shimeji_scroller = Gtk.Template.Child()
     searchbar = Gtk.Template.Child()
+    window_stack = Gtk.Template.Child()
 
     @Gtk.Template.Callback()
     def closing_app(self, user_data):
@@ -202,8 +41,18 @@ class GardenWindow(Adw.ApplicationWindow):
 
     @Gtk.Template.Callback()
     def search_changed(self, entry):
+        results=0
         for shime_group in list(self.shimeji_container):
-            shime_group.set_visible(re.search(entry.get_text(), shime_group.get_child().get_name(), re.IGNORECASE))
+            if re.search(entry.get_text(), shime_group.get_child().get_name(), re.IGNORECASE):
+                shime_group.set_visible(True)
+                results+=1
+        if results==0 and entry.get_text():
+            self.window_stack.set_visible_child_name('no_results')
+        elif len(list(self.shimeji_container)) == 0:
+            self.window_stack.set_visible_child_name('empty')
+        else:
+            self.window_stack.set_visible_child_name('normal')
+
 
     @Gtk.Template.Callback()
     def search_toggle(self, button):
@@ -252,13 +101,14 @@ class GardenWindow(Adw.ApplicationWindow):
                     shutil.rmtree(os.path.join(os.environ['XDG_CACHE_HOME'], 'new_shimeji'))
 
                     self.add_shimeji_selector(shime_name)
+                    self.window_stack.set_visible_child_name('normal')
 
 
     def add_shimeji_selector(self, shime_name:str):
         try:
             if not os.path.isfile(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji', shime_name, 'Shimeji.jar')):
                 raise FileNotFoundError
-            shime_sel = shimeji_selector(shime_name, self)
+            shime_sel = shimeji_widget.shimeji_selector(shime_name, self)
         except Exception as e:
             print(e)
             return
@@ -268,11 +118,14 @@ class GardenWindow(Adw.ApplicationWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        shimeji_widget.window=self
         if not os.path.isdir(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji')):
             os.makedirs(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji'))
         drop_target=Gtk.DropTarget.new(Gdk.FileList, Gdk.DragAction.COPY)
         drop_target.connect('drop', self.on_file_drop)
-        self.shimeji_scroller.add_controller(drop_target)
+        self.window_stack.add_controller(drop_target)
         for shime in os.listdir(os.path.join(os.environ['XDG_DATA_HOME'], 'shimeji')):
             self.add_shimeji_selector(shime)
+        if len(list(self.shimeji_container)) == 0:
+            self.window_stack.set_visible_child_name('empty')
 
